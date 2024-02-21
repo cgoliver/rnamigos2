1f5011288fea290d1105307321b3e2c4d2d697bc
Vincentx15 <vincent.mallet96@gmail.com>
2024-02-21 14:32:19 +0100

    Update violins to group pockets

--- fig_scripts/violins.py
+++ fig_scripts/violins.py
@@ -1,63 +1,86 @@
+import os
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
+import random
+import pickle
 
 from plot_utils import PALETTE
+
+random.seed(42)
+np.random.seed(42)
 
 # TEST SET
 runs = [
     # 'paper_fp.csv',
-    # 'paper_native.csv',
-    # 'paper_dock.csv',
     'fp_split_grouped1.csv',
+    # 'paper_native.csv',
     'native_split_grouped1.csv',
+    # 'paper_dock.csv',
     'dock_split_grouped1.csv',
     'rdock.csv',
     # 'rdock_total.csv',
-    'mixed.csv',
-    'mixed_rdock.csv',
+    # 'mixed.csv',
+    # 'mixed_rdock.csv',
 ]
 names = [
+    # r'\texttt{fp_old}',
     r'\texttt{fp}',
+    # r'\texttt{native_old}',
     r'\texttt{native}',
+    # r'\texttt{dock_old}',
     r'\texttt{dock}',
     r'\texttt{rDock\newline INTER}',
     # r'\texttt{rDock\newline TOTAL}',
-    r'\texttt{mixedold}',
-    r'\texttt{mixedold\newline+ rDock}',
+    # r'\texttt{mixedold}',
+    # r'\texttt{mixedold\newline+ rDock}',
 ]
 #
 # runs = [
-#     # 'dock_split_grouped0.csv',
+#     'dock_split_grouped0.csv',
 #     'dock_split_grouped1.csv',
 #     'paper_dock.csv',
 #     # 'fp_split_grouped0.csv',
-#     'fp_split_grouped1.csv',
-#     'paper_fp.csv',
-#     # 'native_split_grouped0.csv',
+#     # 'fp_split_grouped1.csv',
+#     # 'fp_split_grouped2.csv',
+#     # 'paper_fp.csv',
+#     'native_split_grouped0.csv',
 #     'native_split_grouped1.csv',
+#     'native_split_grouped2.csv',
 #     'paper_native.csv',
 #
 # ]
 # names = [
-#     # r'\texttt{dock0}',
+#     r'\texttt{dock0}',
 #     r'\texttt{dock1}',
 #     r'\texttt{dockold}',
 #     # r'\texttt{fp0}',
-#     r'\texttt{fp1}',
-#     r'\texttt{fpold}',
-#     # r'\texttt{native0}',
+#     # r'\texttt{fp1}',
+#     # r'\texttt{fp2}',
+#     # r'\texttt{fpold}',
+#     r'\texttt{native0}',
 #     r'\texttt{native1}',
+#     r'\texttt{native2}',
 #     r'\texttt{nativeold}',
 # ]
 # decoy_mode = 'pdb'
 decoy_mode = 'chembl'
 # decoy_mode = 'pdb_chembl'
 
+grouped = True
+
 # Parse ef data for the runs and gather them in a big database
 dfs = (pd.read_csv(f"../outputs/{f}") for f in runs)
 dfs = (df.assign(name=names[i]) for i, df in enumerate(dfs))
 big_df = pd.concat(dfs)
+if grouped:
+    script_dir = os.path.dirname(__file__)
+    splits_file = os.path.join(script_dir, '../data/train_test_75.p')
+    _, _, _, test_names_grouped = pickle.load(open(splits_file, 'rb'))
+    group_reps = [random.choice(names) for key, names in test_names_grouped.items()]
+    print(group_reps)
+    big_df = big_df.loc[big_df['pocket_id'].isin(group_reps)]
 big_df = big_df.loc[big_df['decoys'] == decoy_mode].sort_values(by='score')
 
 # For a detailed score per pocket
@@ -75,11 +98,11 @@ sorterIndex = dict(zip(names, range(len(names))))
 means['name_rank'] = means['name'].map(sorterIndex)
 means = means.sort_values(['name_rank'], ascending=[True])
 
-main_palette = PALETTE
-violin_palette = PALETTE
+main_palette = PALETTE + PALETTE
+violin_palette = PALETTE + PALETTE
 
 plt.gca().set_yscale('custom')
-# yticks= np.arange(0.6, 1)
+yticks= np.arange(0.6, 1)
 lower = 0.6
 yticks = [0.6, 0.8, 0.9, 0.95, 0.975, 0.99, 1]
 # lower = 0.
@@ -155,7 +178,7 @@ sns.stripplot(x="name",
               linewidth=1.5,
               data=means)
 
-plt.ylim(0.58, 1.001)
+plt.ylim(lower - 0.02, 1.001)
 plt.xlabel("")
 plt.ylabel("Mean Active Rank")
 plt.grid(True, which='both', axis='y')
