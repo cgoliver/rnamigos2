0d7c75acc3c794d0280d3c68c2c4caf082d194a3
vincent <vincent.mallet96@gmail.com>
2024-11-07 12:25:14 +0100

    add timing script in robin

--- scripts_run/robin_inference.py
+++ scripts_run/robin_inference.py
@@ -13,11 +13,11 @@ import torch
 if __name__ == "__main__":
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-from rnamigos.utils.virtual_screen import raw_df_to_mean_auroc, raw_df_to_efs
+from rnamigos.utils.virtual_screen import raw_df_to_efs, raw_df_to_aurocs
 from rnamigos.utils.graph_utils import load_rna_graph
 from rnamigos.learning.models import get_model_from_dirpath
 from rnamigos.inference import inference_raw, get_models
-from rnamigos.utils.mixing_utils import mix_all
+from rnamigos.utils.mixing_utils import mix_all, mix_two_scores
 
 torch.multiprocessing.set_sharing_strategy("file_system")
 torch.set_num_threads(1)
@@ -33,13 +33,13 @@ POCKET_PATH = "data/json_pockets_expanded"
 
 
 def robin_inference_raw(
-    ligand_name,
-    dgl_pocket_graph,
-    models=None,
-    out_path=None,
-    ligand_cache=None,
-    use_ligand_cache=False,
-    debug=False,
+        ligand_name,
+        dgl_pocket_graph,
+        models=None,
+        out_path=None,
+        ligand_cache=None,
+        use_ligand_cache=False,
+        debug=False,
 ):
     """
     Given the graph pocket and ligand name, as well as models as expected by the inference script,
@@ -115,6 +115,26 @@ def get_all_preds(model, use_rna_fm, swap=0):
     # Finally, compute EFs
     robin_ef_df = raw_df_to_efs(robin_raw_df, fracs=(0.01, 0.02, 0.05, 0.1, 0.2))
     return robin_ef_df, robin_raw_df
+
+
+def timing():
+    """
+    Compute timing when cached ligands are available
+    :return:
+    """
+    robin_lig_pocket_dict, new_to_old = get_swapped_pocketlig()
+    model_dir = "results/trained_models/"
+    models = {"dock_42": "dock/dock_42",
+              "native_42": "is_native/native_42"}
+    models = {name: os.path.join(model_dir, model_path) for name, model_path in models.items()}
+    models = {name: get_model_from_dirpath(model_path) for name, model_path in models.items()}
+    import time
+    t0 = time.time()
+    for ligand_name, pocket_id in robin_lig_pocket_dict.items():
+        raw_df = one_robin(ligand_name, pocket_id, models, use_rna_fm=True)
+        mixed_df = mix_two_scores(raw_df, score1="dock_42", score2="native_42")
+    tf = time.time()
+    print("Total_time to score:", tf-t0)
 
 
 def robin_eval(cfg, model):
@@ -255,7 +275,9 @@ def print_results(swap=0):
         # efs = raw_df_to_efs(raw_df, fracs=(frac,))
         # ef = np.mean(efs['score'].values)
         # print(f"EF@:{frac} {method:<30} \t {ef:>8.4f}")
-        auroc = raw_df_to_mean_auroc(raw_df)
+        df_aurocs = raw_df_to_aurocs(raw_df)
+        # print(df_aurocs['score'].values)
+        auroc = np.mean(df_aurocs['score'].values)
         print(f"AUROC:\t{method:<16}\t{auroc:>10.4f}")
 
 
@@ -263,14 +285,14 @@ if __name__ == "__main__":
     MODELS = {
         "dock_42": "dock/dock_42",
         "native_42": "is_native/native_42",
-        "native_nornafm": "is_native/native_bce0.02_groupsample_nornafm",
+        # "native_nornafm": "is_native/native_bce0.02_groupsample_nornafm",
     }
     PAIRS = {
-        ("rdock", "dock_42"): "dock_rdock",
+        # ("rdock", "dock_42"): "dock_rdock",
         ("native_42", "dock_42"): "rnamigos_42",
-        ("native_nornafm", "dock_42"): "rnamigos_42_nornafm",
+        # ("native_nornafm", "dock_42"): "rnamigos_42_nornafm",
         ("rnamigos_42", "rdock"): "combined_42",
-        ("native_42", "rdock"): "rdocknat_42",
+        # ("native_42", "rdock"): "rdocknat_42",
     }
     SWAP = 0
     # TEST ONE INFERENCE
@@ -283,17 +305,18 @@ if __name__ == "__main__":
     # one_robin(pocket_id, lig_name, model, use_rna_fm=False)
     # assess_overlap_robin()
 
+    # timing()
+
     res_dir = "outputs/robin" if SWAP == 0 else f"outputs/robin_swap_{SWAP}"
     # GET ALL CSVs for the models and plot them
-    """
     get_all_csvs(recompute=False, swap=SWAP)
     get_dfs_docking(swap=SWAP)
     mix_all(res_dir=res_dir, pairs=PAIRS, recompute=False)
-    get_merged_df(recompute=False, swap=SWAP)
+    get_merged_df(recompute=True, swap=SWAP)
     print_results(swap=SWAP)
-    """
 
     # COMPUTE PERTURBED VERSIONS
     for swap in range(1, 4):
-        get_all_csvs(recompute=True, swap=swap)
+        pass
+        # get_all_csvs(recompute=True, swap=swap)
         # mix_all(swap=swap)
